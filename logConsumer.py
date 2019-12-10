4dd29d7ca395800a01ae922215ce49418ecab82e
hhajela <harshit.hajela@gmail.com>
2019-12-10 16:12:56 +0000

    fixes to log consumer

--- logConsumer.py
+++ logConsumer.py
@@ -2,28 +2,27 @@ from kafka import KafkaConsumer
 
 
 def run_consumer(topic_name):
-    logf = None
     try:
-        consumer = KafkaConsumer(topic_name, auto_offset_reset='latest', \
+        consumer = KafkaConsumer(topic_name, auto_offset_reset='latest',\
                 bootstrap_servers=['10.168.0.2:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
-        logf = open('RTAL.log','a')
         
-        for msg in consumer:
-            logf.write(msg.value)
-        
+        while(True):
+            for msg in consumer:
+                with open('RTAL.log','a') as logf:
+                    logf.write('{0}\n'.format(msg.value.decode('utf-8')))
+        """
         while True:
             if not consumer:
                 sleep(20)
             for msg in consumer:
-                log.info("printing the mesg with key %s " %msg.key)
-                print(msg.value)
-        
+                logf.write(msg.value.decode('utf-8'))
+        """
+    except KeyboardInterrupt:
+        pass
     except Exception as e:
-        logf.write("Exception {0} occurred".format(e))
+        print("Exception {0} occurred".format(e))
     finally:
         consumer.close()
-        if logf is not None:
-            logf.close()
 
 if __name__=="__main__":
     run_consumer('alog')
