93ee41204dc3157402811caea3d8e9cd396de91b
nikhiljain217 <nikhiljain217@gmail.com>
2019-12-10 18:58:54 +0000

    Final

--- consumer_faust.py
+++ consumer_faust.py
@@ -7,7 +7,7 @@ import json
 app = faust.App('consumer',broker='kafka://10.168.0.2:9092',value_serializer='raw', consumer_auto_offset_reset="latest")
 from bson import ObjectId
 
-topic_log = app.topic("alog")
+log_topic = app.topic("alog")
 
 tz = pytz.timezone('Asia/Shanghai')
 mongo_topic = app.topic('mongo')
@@ -23,13 +23,11 @@ def convert_document(data):
     return document
 
 def find_stat(taxi):
-    log_topic.send("Will find the stats now")
     last_entry = mycol.find_one({"taxi_id": int(taxi[0])})
     
     #If this the first entry of the taxi
     if last_entry is None:
         return [0,0,0]
-    log_topic.send("Taxi%s found in tht databse" % str(taxi[0]))
     last_cursor = mycol.find({"taxi_id": int(taxi[0])},{"_id":0,"timestamp":1,"geo_point.lat":1,\
             "geo_point.lon":1,"distance":1,"halt_time":1,"mph":1}).limit(1).sort([("_id",-1)])
     last=dict()
@@ -41,8 +39,6 @@ def find_stat(taxi):
     distance = last['distance']
     time_difference = taxi[1]-last['timestamp']
     halt_time = last['halt_time']
-    print("origin is",origin,taxi[0])
-    print("destination is ",dest,taxi[0])
     if origin != dest:
         added_distance = calculate_distance(origin,dest)
         time_hr = time_difference/3600
@@ -57,7 +53,6 @@ def find_stat(taxi):
         speed = 0
         halt_time+=time_difference
 
-    topic_logs.send("updating the following values %s , %s and %s", str(speed),str(distance),str(halt_time))
     return [speed,distance,halt_time]
 
 def convert_time_epoch(regular_date):
@@ -75,13 +70,10 @@ async def greet(mongo_topic):
     async for taxi in mongo_topic:
         global mycol
         taxi = taxi.decode('utf-8').split(",")
-        await log_topic.send("Consumer - new entry %s", str(taxi))
 
         taxi[1]=convert_time_epoch(taxi[1])
         taxi.extend(find_stat(taxi))
         doc = convert_document(taxi)
-        await log_topic.send("Consumer - push entry %s" % str(doc))
         doc_json = json.dumps(doc)
         await elastic_topic.send(value=doc_json)
         mycol.insert(doc)
-        await log_topic.send("Consumer - Entry is sent")
--- producer.py
+++ producer.py
@@ -5,13 +5,43 @@ import logging
 import traceback
 from kafka import KafkaProducer
 
-def send_log(message)
+
+logger=None
+def set_log():
+    try:
+        global logger
+        if logger:
+            return logger
+
+
+        logger = logging.getLogger('producer')
+        # create file handler which logs even debug messages
+        logger.setLevel(logging.INFO)
+        fh = logging.FileHandler('producer.log')
+        # create console handler with a higher log level
+        #ch = logging.StreamHandler()
+        #ch.setLevel(logging.ERROR)
+        # create formatter and add it to the handlers
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        #ch.setFormatter(formatter)
+        # add the handlers to the logger
+        logger.addHandler(fh)
+        #logger.addHandler(ch)
+        return logger
+    except Exception:
+        print("problem while setting logger")
+        traceback.print_exc()
+
+
+
+def send_log(message,_producer):
     try:
         topic_log = "alog"
-        _producer  = connect_kafka_producer()
         
-        publish_message(_producert,topic_log, 1, message)
-
+        key_bytes = bytes("1", encoding='utf-8')
+        value_bytes = bytes(message, encoding='utf-8')
+        _producer.send(topic_log, key=key_bytes, value=value_bytes)
 
     except Exception:
         print("problem while setting logger")
@@ -23,15 +53,13 @@ def send_log(message)
 def publish_message(producer_instance, topic_name, key, value):
     try:
         log = set_log()
-        send_log("Producer- Publish Message -  key %s "%key)
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(','.join(value), encoding='utf-8')
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
         producer_instance.flush()
-        send_log("Producer Publish successfully ")
     except Exception as e:
-        send_log("Error in publishing message for %s",key)
-        send_log("Error for failure",str(e))
+        log.error("Error in publishing message for %s",key)
+        log.error("Error for failure",str(e))
 
 def connect_kafka_producer():
     _producer = None
@@ -39,8 +67,8 @@ def connect_kafka_producer():
         log = set_log()
         _producer = KafkaProducer(bootstrap_servers=['10.168.0.2:9092'], api_version=(0, 10))
     except Exception as ex:
-        send_log('Producer - Exception while Creating Producer object')
-        log.error("Above Exception us"str(ex))
+        log.error('Producer - Exception while Creating Producer object')
+        log.error("Above Exception is",str(ex))
     finally:
         return _producer
 
@@ -50,15 +78,18 @@ def get_taxis():
         log = set_log()
         #create producer instance
         _producer = connect_kafka_producer()
+    except Exception as e:
+        log.error("Error while getting the taxis")
+        log.error(str(e))
+    try:
         topic_name1 = "mongo"
-        send_log("Producer - Successfully created the producer instance")
+        send_log("Producer - Successfully created the producer instance",_producer)
         data_file = open("sorted_new.csv","r")
-        send_log("Producer -Successfully opened the data file")
+        send_log("Producer -Successfully opened the data file",_producer)
         line = data_file.readline().strip()
         line = line.split(',')
         start_time = datetime.datetime.strptime(line[2],'%Y-%m-%d %H:%M:%S')
         end_time = start_time+datetime.timedelta(minutes=1)
-        send_log("Producer - Will publish data between %s and %s"% (str(start_time), str(end_time)))
         publish_message(_producer, topic_name1,line[0],line[1:])
         for line in data_file:
             line = line.strip().split(',')
@@ -71,7 +102,7 @@ def get_taxis():
 
 
     except Exception as e:
-        log.error("Error while getting the taxis")
-        log.error(str(e))
+        send_log("Error while getting the taxis")
+        send_log(str(e))
 if __name__ =='__main__':
     get_taxis()
